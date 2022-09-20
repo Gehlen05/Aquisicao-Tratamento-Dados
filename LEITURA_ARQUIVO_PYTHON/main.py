f28fe10708bcc41b63bf9dc31653b930e543929c
alexsandrogehlen <alexsandrololo@hotmail.com>
2022-09-20 18:32:52 -0300

    alteracoes nas medias

--- LEITURA_ARQUIVO_PYTHON/main.py
+++ LEITURA_ARQUIVO_PYTHON/main.py
@@ -1,34 +1,98 @@
+# https://github.com/Gehlen05/Aquisicao-Tratamento-Dados
 import pandas as pd
 import matplotlib.pyplot as plt
-batimentos_dia = {}
-# arquivo_entrada = ('heart_rate_short.csv')
-arquivo_entrada = 'heart_rate_long.csv'
-heart_rate = pd.read_csv(arquivo_entrada)
-df = pd.DataFrame(heart_rate)
-mean_df = df['heartRate'].mean()
-desvio_df = df['heartRate'].std()
-print(f'Media e: {mean_df}')
-print(f'Desvio padrao e: {desvio_df}')
-eixo_y = df['heartRate'].rolling(1200).mean()
-x = (range(len(eixo_y)))
-plt.plot(x, eixo_y)
-plt.title('BPM por dia')
-plt.savefig(f'imagens/serie_temporal')
-plt.cla()
-date = df['date'].apply(lambda x: str(x))
-eixo_x = df['time']
-eixo_y = df['heartRate']
-i = 0
-for dias in date:
-    batimentos_dia.setdefault(dias, []).append(eixo_y[i])
-    i += 1
-for dia, batimentos in batimentos_dia.items():
-    df = pd.DataFrame(batimentos)
-    batimentos_filtrados = df.rolling(60).mean()
-    x = (range(len(batimentos_filtrados)))
-    plt.plot(x, batimentos_filtrados)
-    plt.title('BPM por dia')
-    plt.xlabel('Dia')
+import statistics
+
+
+def _ler_arquivo(arquivo_entrada):
+    heart_rate = pd.read_csv(arquivo_entrada)
+    data_frame = pd.DataFrame(heart_rate)
+    return data_frame
+
+
+def _separar_dia(data_frame):
+    batimentos_diarios = {}
+    datas = data_frame['date']
+    i = 0
+    batimentos = data_frame['heartRate']
+    for data in datas:
+        batimentos_diarios.setdefault(data, []).append(batimentos[i])
+        i += 1
+    return batimentos_diarios
+
+
+def _plotar_grafico(eixo_x, eixo_y, nome, titulo='Batimentos Dia'):
+    plt.plot(eixo_x, eixo_y)
+    plt.title(titulo)
+    plt.xlabel('Tempo')
     plt.ylabel('BPM')
-    plt.savefig(f'imagens/{dia}')
+    plt.savefig(f'imagens/{nome}')
     plt.cla()
+
+
+def _todos_dias_media_movel(data_frame):
+    mean_data_frame = data_frame['heartRate'].mean()
+    desvio_data_frame = data_frame['heartRate'].std()
+    print(f'Media e: {mean_data_frame}')
+    print(f'Desvio padrao e: {desvio_data_frame}')
+    eixo_y = data_frame['heartRate'].rolling(86400).mean()
+    x = (range(len(eixo_y)))
+    _plotar_grafico(x, eixo_y, 'serie_temporal_media_movel_diaria')
+
+
+def _media_total_(data_frame):
+    batimentos_diarios = _separar_dia(data_frame)
+    batimento_media_diaria = []
+    for data, batimento in batimentos_diarios.items():
+        batimento_media_diaria.append(statistics.mean(batimento))
+    _plotar_grafico(batimentos_diarios.keys(), batimento_media_diaria, 'temporal_media_statistics_dia')
+
+
+def _media_diaria_minuto(data_frame):
+    batimentos_diarios = _separar_dia(data_frame, 'time')
+    for dia, batimentos in batimentos_diarios.items():
+        data_frame = pd.DataFrame(batimentos)
+        batimentos_filtrados = data_frame.rolling(60).mean()
+        x = (range(len(batimentos_filtrados)))
+        _plotar_grafico(x, batimentos_filtrados, dia)
+
+def _media_diaria_hora(data_frame):
+    batimentos_diarios = {}
+    datas = data_frame['date']
+    hora = data_frame['time'].astype(str)
+    batimentos = data_frame['heartRate']
+    i = 0
+    for data in datas:
+        batimentos_diarios.setdefault(data, {'hora': [], 'batimentos': []})
+        batimentos_diarios[data]['batimentos'].append(batimentos[i])
+        batimentos_diarios[data]['hora'].append(hora[i][:2])
+        i += 1
+    for dia, dados in batimentos_diarios.items():
+        data_frame_batimentos = dados['batimentos']
+        data_frame_hora = dados['hora']
+        for hora in data_frame_hora:
+            horarios = {}
+            i = 0
+            for hora in data_frame_hora:
+                # print(type(hora))
+                # print(hora)
+                horarios.setdefault(hora, []).append(data_frame_batimentos[i])
+                i += 1
+            batimentos_hora = []
+            for hora, batimentos in horarios.items():
+                batimentos_hora.append(statistics.mean(batimentos))
+            _plotar_grafico(horarios.keys(), batimentos_hora, f'{dia}_hora')
+
+        # hora = list(range(0, 24, 1))
+        # batimentos_filtrados = data_frame_batimentos.rolling(3600).mean()
+        # print(len(data_frame_batimentos))
+        # print(len(batimentos_filtrados))
+        # # x = (range(len(batimentos_filtrados)))
+
+
+if __name__ == '__main__':
+    data_frame = _ler_arquivo('heart_rate_long.csv')
+    # _todos_dias_media_movel(data_frame)
+    # _media_total_(data_frame)
+    # _media_diaria_minuto(data_frame)
+    _media_diaria_hora(data_frame)
